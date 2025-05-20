4ae3ec22ef2eec87529bb9343f7fca1397852a51
rahma <sabrinafitri593@gmail.com>
2025-05-20 10:51:21 +0700

    implementasi

--- demoKelas.java
+++ demoKelas.java
@@ -8,14 +8,16 @@ public class demoKelas {
         k.tambahDiBelakang(new siswa ("Riska"));
         k.tambahDiBelakang(new siswa("Amanda"));
         k.tambahDiBelakang(new siswa("Ainun"));
-        k.tambahDiBelakang(new siswa("Mutara"));
-        k.tambahSetelah("Riska", new siswa ("Azmi"));
+        k.tambahDiBelakang(new siswa("Tiara"));
         k.hapus("Riska");
         k.tampilkansemua(); 
 
         System.out.println("jumlah siswa: " + k.jumlahsiswa());
         System.out.println("urutan Riska: " + k.urutan("Riska"));
         System.out.println("siswa di index 2: " + k.ambilDi(2).nama);
- 
+
+        k.hapus("Riska");
+        System.out.println("setelah hapus Riska:");
+        k.tampilkansemua();
     }
 }
